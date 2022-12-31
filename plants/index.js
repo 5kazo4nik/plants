4670ab770545b277a535bbcbaa9cef9123eaf1aa
Oleg <alhimov.oleg2012@yandex.ru>
2022-12-31 14:26:49 +0300

    feat: add mobile adaptability

--- plants/index.js
+++ plants/index.js
@@ -1,1 +1,35 @@
-console.log('1. Верстка валидная: +10;\n2. Верстка сематическая: +20;\n3. Верстка соответсвует макету: +48;\n4. Требования к CSS: +12;\n5. Интерактивность реализуемая через CSS: +20\nИтого: 100');
+console.log(
+  '1. Вёрстка соответствует макету. Ширина экрана 768px: +24;\n2. Вёрстка соответствует макету. Ширина экрана 380px: +24;\n3. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется: +15;\n4. На ширине экрана 380рх и меньше реализовано адаптивное меню: +22\nИтого: 75'
+);
+
+const navList = document.querySelector('.nav-list');
+const navListBg = document.querySelector('.nav-list-bg');
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerLine1 = document.querySelector('.burger-line1');
+const burgerLine2 = document.querySelector('.burger-line2');
+
+burgerBtn.addEventListener('click', () => {
+  burgerLine1.classList.toggle('burger-line1-active');
+  burgerLine2.classList.toggle('burger-line2-active');
+  navList.classList.toggle('nav-list-active');
+  navListBg.classList.toggle('nav-list-bg-active');
+  document.body.classList.toggle('body-lock');
+
+  navListBg.addEventListener('click', () => {
+    burgerLine1.classList.remove('burger-line1-active');
+    burgerLine2.classList.remove('burger-line2-active');
+    navList.classList.remove('nav-list-active');
+    navListBg.classList.remove('nav-list-bg-active');
+    document.body.classList.remove('body-lock');
+  });
+
+  [...document.getElementsByClassName('nav-item')].forEach((link) => {
+    link.addEventListener('click', () => {
+      burgerLine1.classList.remove('burger-line1-active');
+      burgerLine2.classList.remove('burger-line2-active');
+      navList.classList.remove('nav-list-active');
+      navListBg.classList.remove('nav-list-bg-active');
+      document.body.classList.remove('body-lock');
+    });
+  });
+});
